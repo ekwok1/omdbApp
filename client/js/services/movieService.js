0a77d143b9ea576017b620f7119e106f40c2301b
eugene.kwok <eugene.kwok@workday.com>
2017-04-13 10:36:01 -0700

    add support for query type

--- client/js/services/movieService.js
+++ client/js/services/movieService.js
@@ -7,13 +7,25 @@
 	const OMDB_API_SEARCH_ENDPOINT="http://www.omdbapi.com/?s=";
 	const OMDB_API_ID_ENDPOINT="http://www.omdbapi.com/?i=";
 	const TYPE = {
-		MOVIE: "&type=movie"
+		MOVIE: "&type=movie",
+		SERIES: "&type=series",
+		EPISODE: "&type=episode"
 	}
 
 	function service($http) {
 		this.search = (query) => {
 			var moviesArr = [];
-			$http.get(OMDB_API_SEARCH_ENDPOINT + query.searchQuery + TYPE.MOVIE)
+
+			var path = OMDB_API_SEARCH_ENDPOINT + query.searchQuery;
+			if (query.queryType == "episode") {
+				path += TYPE.EPISODE;
+			} else if (query.queryType == "series") {
+				path += TYPE.SERIES;
+			} else {
+				path += TYPE.MOVIE;
+			}
+
+			$http.get(path)
 				.then(movies => {
 					movies.data.Search.forEach(movie => {
 						moviesArr.push(movie);
